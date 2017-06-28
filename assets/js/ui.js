0d51f6c50ff1cdadcb8a9a03ce30999e8563e8c9
Saveliy Samoylov <samoylov3@hotmail.com>
2017-06-28 14:16:07 -0400

    created some UI relevant javascript: Sticky SearchBar and Star Ratings

---
+++ assets/js/ui.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+	 //================================ Star Ratings ================================
+
+		var starsRating = 4.5/5;
+		var starsWidthNum = 90;
+		var newWidth = starsRating * starsWidthNum;
+		var starsWidth = $(".card--Rating__Overlay").css("width", newWidth+"px");
+
+	//===============================================================================
+
+	//================================= Scroll Events ===============================
+
+	 $(document).on("scroll", function(){
+	 	var scrollPosition = window.pageYOffset;
+	 	//console.log(window.pageYOffset);
+
+	 	listenScrollDown();
+
+	 	function listenScrollDown(){
+	 			 	
+		 	if (scrollPosition > 263){
+		 		$(".header--Search").css({
+		 			position:"fixed",
+		 			top:"0",
+		 			left:"90px",
+		 			width:"100%",
+		 			height:"69px",
+		 			transition: ".2s",
+		 			borderRadius: "0",
+		 			borderBottom: "1px solid #a9c7ce",
+		 			boxShadow: "none",
+		 		})
+
+		 		$(".header--Search__Input").css({
+		 			width:"294px",
+		 			height: "67px",
+		 			borderRadius: "0",
+		 			borderRight: "1px solid #dbdee3",
+		 		})
+
+		 		$(".header--Interface").css({
+					position:"fixed",
+					right: "10px",
+		 		})
+
+		 	} else{
+		 		listenScrollUp();
+		 	}
+	 	}
+
+	 	function listenScrollUp(){
+	 		
+	 		if (scrollPosition <= 263){
+
+	 		    $(".header--Search").css({
+					position: "absolute",
+					left:"calc(50% - 294px)",
+					bottom:"-35px",
+					height: "70px",
+					width:"588px",
+					borderRadius:"5px",
+					border:"none",
+					top:"",
+					"-moz-box-shadow":  "0 5px 3px #b7d1d6",
+                    "-webkit-box-shadow":  "0 5px 3px #b7d1d6",
+                    "box-shadow":          "0 5px 3px #b7d1d6",
+		 		})
+
+		 		$(".header--Search__Input").css({
+		 			width:"50%",
+		 			height: "70px",
+		 			borderRadius: "5px",
+		 			borderRight: "1px solid #dbdee3",
+		 		})
+
+		 		$(".header--Interface").css({
+					position:"relative",
+		 		})
+	 		}
+	 	}
+
+	 });
+	 
+	 //===============================================================================
+
+	});
