82c8687abaf1ca0bd3cd85d92c9cdd3cf141b19f
johnsaugy <john.saugy@gmail.com>
2017-06-28 19:50:10 -0400

    Update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-var searchTerm = document.getElementsByClassName("searchTerm")[0];
-var searchLocation = document.getElementsByClassName("searchLocation")[0];
-var searchLocation = document.getElementsByClassName("searchLocation")[0];
+var searchTerm = document.getElementsByClassName("header--Search__Input")[0];
+var searchLocation = document.getElementsByClassName("header--Search__Input")[0];
+var searchLocation = document.getElementsByClassName("header--Search__Input")[0];
 var runFunc = document.getElementsByClassName("btn6")[0];
 var results = document.getElementsByClassName("results")[0];
 
@@ -34,17 +34,17 @@ $.ajax({
 
 function yelpApi(business) {
     var searchResults = document.getElementsByClassName("results")[0];
-    var resultDiv = "<a target='_blank' href = ' "+ business.url + "  '><div class='output'><img src='";
+    var resultDiv = "<a target='_blank' href = ' "+ business.url + "  '><div class='card--Result'><img src='";
     resultDiv += business.image_url;
     resultDiv += "'> ";
-            resultDiv += "<div class='name'>";
+            resultDiv += "<div class='card--Title'>";
             resultDiv += business.name;
                 resultDiv += "<div class='category'>";
              	resultDiv += business.categories[0]
                 resultDiv += "<div class='location'>";
                 resultDiv += business.location.address + ", " + business.location.city+", " + business.location.state_code + " " + business.location.postal_code;
                 resultDiv += "</div>";
-    			resultDiv += "<div class='rating'>";
+    			resultDiv += "<div class='card--Rating'>";
              	resultDiv += business.rating
              	resultDiv += "</div>";
              	resultDiv += "<div class='snippet'>";
